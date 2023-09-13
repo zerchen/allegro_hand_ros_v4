c9639c5caab5b4c5cac8792c4595e68bfdfef0e4
EtienneAr <etienne.arlaud@gmail.com>
2023-09-13 15:08:15 +0200

    Renaming joint alphabetically

--- src/allegro_hand_controllers/src/allegro_node.cpp
+++ src/allegro_hand_controllers/src/allegro_node.cpp
@@ -14,9 +14,9 @@ static inline std::string &rtrim(std::string &s)
 
 std::string jointNames[DOF_JOINTS] =
         {
-                "joint_0", "joint_1", "joint_2", "joint_3",
-                "joint_4", "joint_5", "joint_6", "joint_7",
-                "joint_8", "joint_9", "joint_10", "joint_11",
+                "joint_00", "joint_01", "joint_02", "joint_03",
+                "joint_04", "joint_05", "joint_06", "joint_07",
+                "joint_08", "joint_09", "joint_10", "joint_11",
                 "joint_12", "joint_13", "joint_14", "joint_15"
         };
 
