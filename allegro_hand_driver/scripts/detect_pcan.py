a1f4f6b0697faa523b854cede7fc9907761bf921
EtienneAr <>
2023-12-15 11:39:30 +0100

    Auto can detection also look for PCI iface

--- allegro_hand_driver/scripts/detect_pcan.py
+++ allegro_hand_driver/scripts/detect_pcan.py
@@ -10,22 +10,30 @@
 
 import glob
 import re
+import sys
 
-def pcan_search():
+def pcan_search(interface="usb"):
     # Look for all possible file matches.
-    filelist = glob.glob('/dev/pcanusb*')
+    filelist = glob.glob(f'/dev/pcan{interface}*')
 
     # Search for the correct format: pcanusbN
-    device_re = re.compile('.*pcanusb[0-9]+')
+    device_re = re.compile(f'.*pcan{interface}[0-9]+')
 
     gen = (x for x in filelist if device_re.search(x))
     valid_files = [x for x in gen]
 
-    if len(valid_files) == 1:
-        print(valid_files[0])
-        return valid_files[0]
-    else:
-        return ''
+    return valid_files
 
 if __name__ == '__main__':
-    pcan_search()
+    search_list = ['usb', 'pci']
+    available_list = []
+
+    for iface in search_list:
+        available_list += pcan_search(iface)
+
+    if(not available_list):
+        print(f"No pcan device found.", file=sys.stderr)
+    if(len(available_list) > 0):
+        if(len(available_list) > 1):
+            print(f"More than one pcan device available, taking the first one of: {available_list}", file=sys.stderr)
+        print(available_list[0])
